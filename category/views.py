6528bb00abc6625717f4b40f940aa4dc490cced3
Shine Mon <shine@richkenmedia.com>
2021-09-05 20:21:08 +0530

    example for query stirng and url param

--- category/views.py
+++ category/views.py
@@ -7,5 +7,10 @@ def index(request):
     return Response({'message': 'Index Function'})
 
 @api_view(['GET'])
-def show(request):
-    return Response({'message': request.query_params})
+def show(request, id):
+    return Response({'message': "Show function will get values from url"})
+
+
+@api_view(['GET'])
+def search(request):
+    return Response({'message': "Search function will get values from querystring"})
