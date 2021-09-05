5e4ba21c73f42759f450a00b736a06a4c53aaaf3
Shine Mon <shine@richkenmedia.com>
2021-09-05 19:53:18 +0530

    added swagger documenatin based on DRF documentation

--- category/views.py
+++ category/views.py
@@ -5,3 +5,7 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def index(request):
     return Response({'message': 'Index Function'})
+
+@api_view(['GET'])
+def show(request):
+    return Response({'message': request.query_params})
--- drf/urls.py
+++ drf/urls.py
@@ -16,10 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.http import response
 from django.urls import path,include
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('', lambda self: response.HttpResponse('DRF API Server') ),
     path('admin/', admin.site.urls),
     path('category/', include('category.urls')),
     path('api-auth/', include('rest_framework.urls')),
+
+    # Swaggar Related Routes
+    path('openapi', get_schema_view(
+        title="Your Project",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 ]
